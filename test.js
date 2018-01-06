c2a1ab510e7c0912747d7c5998e2e5efd1e5f583
k0n1star <konstantinpandl@yahoo.de>
2018-01-07 02:57:35 +0800

    initial 2

---
+++ test.js
@@ -0,0 +1,5 @@
+var zapi = require("./zapi.js");
+
+var zapi1=new zapi(); 
+
+console.log(zapi1.accountRequest('t3Vz22vK5z2LcKEdg16Yv4FFneEL1zg9ojd'))
--- zapi.js
+++ zapi.js
@@ -1,2 +1,24 @@
-var zchain_api = require('zchain-api').zchain();
+var method = zapi.prototype;
 
+const request = require('request');
+
+function zapi() {
+	
+}
+
+method.accountRequest = function(address) {
+    request('https://api.zcha.in/v2/mainnet/accounts/'+address+'/recv?limit=20&offset=0', { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        else {
+			return console.log(body);
+        }
+    });
+};
+
+// ToDo: if result contains more than 20 outputs, detect it and take appropriate action
+
+// zapi.accountRequest('t3Vz22vK5z2LcKEdg16Yv4FFneEL1zg9ojd')
+
+module.exports = zapi;
